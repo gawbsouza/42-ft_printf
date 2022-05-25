7e36f8096098d7ca8c32a2cd934afa9b470c671a
gabriel <gabriel.souza@zoho.com>
2022-05-25 15:06:12 -0300

    ft_printf.h: remover espaço em branco

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 15:58:38 by gasouza           #+#    #+#             */
-/*   Updated: 2022/05/25 11:40:53 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/05/25 15:05:43 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,5 @@ int		ft_print_hex(unsigned int n, int upper);
 int		ft_print_num_base(long long int n, int signal, char *base);
 int		ft_print_ptr(void *ptr);
 int		ft_print_str(const char *str);
-	
+
 #endif
